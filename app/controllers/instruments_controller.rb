c5fca0dd98284883c10eb1be7c60b3e27c894828
Flavia Muntean <muntean.flavia@gmail.com>
2019-08-26 15:49:40 +0100

    instruments controller and routes init

---
+++ app/controllers/instruments_controller.rb
@@ -0,0 +1,53 @@
+class InstrumentsController < ApplicationController
+  before_action :set_instrument, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:create]
+  def index
+    @instruments = Instrument.all
+  end
+
+  def show
+  end
+
+  def new
+    @instrument = Instrument.new
+  end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
+    @instrument.user = @user
+    if @instrument.save
+      redirect_to user_instruments_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @instrument.update(instrument_params)
+      redirect_to instrument_path(@instrument)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @instrument.destroy
+  end
+
+  private
+
+  def instrument_params
+    params.require(:instrument).permit(:name, :photo, :price_per_day, :user)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_instrument
+    @instrument = Instrument.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user do
+    resources :instruments, only: [:new, :create, :edit, :update]
+  end
+
+  resources :instruments, only: [:index, :show, :destroy]
 end
